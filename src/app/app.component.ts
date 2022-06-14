1212dbf7bb7dac8b3abfee5b915d3bd965913b15
prana <kriti.rithanya@gmail.com>
2022-06-14 17:10:55 +0530

    Added my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Subscriber, Subscription } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'method';
+  constructor(private http:HttpClient){
+         
+  }
+  onSubmit(data: any){
+    
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      // 'responseType': 'json',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Headers':'Content-type',
+      
+      
+      
+  });
+ 
+        
+        
+    this.http.post('https://aol8m5c62j.execute-api.ap-south-1.amazonaws.com/dev/user/create',data,{headers: headers }).subscribe((result)=>
+    console.warn(result))
+    console.warn(data)
+  }
 }
