d481c3aa00619ef8fc40e8cdbf1c3d211aa88f2f
kritisasikumar <100333115+kritisasikumar@users.noreply.github.com>
2022-06-21 11:41:01 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,70 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Subscriber, Subscription } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { getLocaleDateFormat } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+ 
+  
+
   constructor(private http:HttpClient){
+    
          
   }
   onSubmit(data: any){
+    // data.sum=(data.firstName*data.lastName)
+    // console.log(data.sum);
     
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      // 'responseType': 'json',
-      'Access-Control-Allow-Origin':'*',
-      'Access-Control-Allow-Headers':'Content-type',
-      
-      
-      
-  });
- 
-        
-        
-    this.http.post('https://aol8m5c62j.execute-api.ap-south-1.amazonaws.com/dev/user/create',data,{headers: headers }).subscribe((result)=>
-    console.warn(result))
-    console.warn(data)
+    this.http.post('https://49yewrmj8f.execute-api.ap-south-1.amazonaws.com/dev/user/create',data)
+    .subscribe((result:any)=>{
+      const token = result
+      console.log(token)
+      sessionStorage.setItem('token', JSON.stringify(result.token));
+    })
+
+  }  
+  onLogin(data: any){
+
+    this.http.post('https://49yewrmj8f.execute-api.ap-south-1.amazonaws.com/dev/user/login',data)
+    .subscribe((result:any)=>{
+      const token = result
+      console.log(token)
+      sessionStorage.setItem('token', JSON.stringify(result.token));
+    })
+
+  }  
+  getData(){
+    // console.log(sessionStorage.getItem('token'));
+   
+    var ses=sessionStorage.getItem('token')!;
+    console.log(ses)
+    const headers = { 'authorizationToken': ses}
+    this.http.get<any>('https://49yewrmj8f.execute-api.ap-south-1.amazonaws.com/dev/users', { headers }).subscribe(result => {
+        console.log(result)
+    })
+    
   }
+
+  Update(){
+    var ses=sessionStorage.getItem('token')!;
+    const headers = { 'authorizationToken': ses}
+    this.http.put('https://49yewrmj8f.execute-api.ap-south-1.amazonaws.com/dev/user/{id}', { headers }).subscribe(result => {
+        console.log(result)
+    })
+
+  }
+ Delete(){
+  var ses=sessionStorage.getItem('token')!;
+    const headers = { 'authorizationToken': ses}
+    this.http.delete('https://49yewrmj8f.execute-api.ap-south-1.amazonaws.com/dev/user/{id}', { headers }).subscribe(result => {
+        console.log(result)
+    })
+ }
 }
